f31f699baad53dfe24cad5b2c11a533df48b55ec
MichalSamsonowski <msamsonowski01@onet.pl>
2022-04-23 01:28:37 +0200

    Tworzenie 100 plików z numerem nazwa i data

--- skrypt.sh
+++ skrypt.sh
@@ -5,3 +5,14 @@ if [ " $1 "  ==  " --date " ] ;
 następnie
 echo  $dzisiaj
 fi
+
+if [ " $1 "  ==  " --logs " ] ;
+następnie
+b=100
+dla  i  w  $( seq 100 )
+robić
+	echo log $i  >> log $i .txt
+	echo  $0  >> log $i .txt
+	echo  $dzisiaj  >> log $i .txt
+zrobione
+fi
